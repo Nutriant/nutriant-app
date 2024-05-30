dfe4f1b62217c56a8d90b1c7d46b19a42bc78775
colondum <decarabian21@gmail.com>
2024-05-30 13:06:29 +0700

    setup retrofit

---
+++ app/src/main/java/com/dicoding/nutrient/api/ApiService.kt
@@ -0,0 +1,4 @@
+package com.dicoding.nutrient.api
+
+interface ApiService {
+}
---
+++ app/src/main/java/com/dicoding/nutrient/api/RetrofitClient.kt
@@ -0,0 +1,27 @@
+package com.dicoding.nutrient.api
+
+import com.dicoding.nutrient.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = ""
+    val instance: ApiService by lazy {
+        val loggingInterceptor = if(BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
