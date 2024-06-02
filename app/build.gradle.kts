d66e4eeb203b90236801ef6d73ad3a7eb534d903
manueleric32 <manueleric.saputra@gmail.com>
2024-06-03 00:20:28 +0700

    Push profile fragment and custom pop up button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,8 @@ dependencies {
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Circle Image View
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
 }
--- app/src/main/java/com/dicoding/nutrient/ui/fragments/dashboard/ProfileFragment.kt
+++ app/src/main/java/com/dicoding/nutrient/ui/fragments/dashboard/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.dicoding.nutrient.ui.fragments.dashboard
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,13 +8,43 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dicoding.nutrient.R
+import com.dicoding.nutrient.databinding.ActivityProfileFragmentBinding
+import com.dicoding.nutrient.databinding.CustomPopupDialogBinding
+import com.google.android.material.button.MaterialButton
 
 class ProfileFragment : Fragment() {
+    private var _binding: ActivityProfileFragmentBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.activity_profile_fragment, container, false)
+    ): View {
+        _binding = ActivityProfileFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.layoutSignOut.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(requireContext())
+        val dialogBinding = CustomPopupDialogBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+        dialog.show()
+
+        dialogBinding.btDialogCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialogBinding.btDialogLogout.setOnClickListener {
+            // Code for logout
+            dialog.dismiss()
+        }
     }
 }
