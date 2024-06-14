8e1d2b8366459e77e6ccd65cc5686bc8ab147687
manueleric32 <manueleric.saputra@gmail.com>
2024-06-14 13:39:34 +0700

    Update searchitem xml and binding to Information Log

--- app/src/main/java/com/dicoding/nutrient/ui/adapters/SearchFoodFatsecretAdapter.kt
+++ app/src/main/java/com/dicoding/nutrient/ui/adapters/SearchFoodFatsecretAdapter.kt
@@ -1,5 +1,6 @@
 package com.dicoding.nutrient.ui.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -8,11 +9,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.nutrient.data.model.response.fatsecret.AllFood
 import com.dicoding.nutrient.data.model.response.fatsecret.GetSearchFoodResponse
 import com.dicoding.nutrient.databinding.ItemSearchFoodFatsecretBinding
+import com.dicoding.nutrient.ui.activities.InformationLogActivity
 
 class SearchFoodFatsecretAdapter
     : ListAdapter<AllFood, SearchFoodFatsecretAdapter.ViewHolder>(DIFF_CALLBACK) {
 
-    inner class ViewHolder(var binding: ItemSearchFoodFatsecretBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(var binding: ItemSearchFoodFatsecretBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val clickedFood = getItem(position)
+                    val intent = Intent(itemView.context, InformationLogActivity::class.java)
+                    // Berikan put extra disini
+                    itemView.context.startActivity(intent)
+                }
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemSearchFoodFatsecretBinding.inflate(LayoutInflater.from(parent.context), parent, false)
